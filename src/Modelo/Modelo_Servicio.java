29ea0365afe69107e91a9af3b00e899605b7ba6f
Asus <Asus@DESKTOP-UI4PVPK>
2023-03-09 18:14:40 -0500

    Tatiana crea ModeloServicio-ArreglaServic bnvvcbbbmkmnnmj

--- src/Modelo/Modelo_Servicio.java
+++ src/Modelo/Modelo_Servicio.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Taataiana Lalvay
+ * @author Taataiana 
  */
 public class Modelo_Servicio extends Servicio {
 
--- src/Modelo/Servicio.java
+++ src/Modelo/Servicio.java
@@ -8,7 +8,7 @@ import java.sql.Date;
 
 /**
  *
- * @author Usuario
+ * @author Tatiana
  */
 public class Servicio {
 
