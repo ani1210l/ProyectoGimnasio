4211448d01e5ba05eee80e2a0483905ee975a428
Asus <Asus@DESKTOP-UI4PVPK>
2023-03-09 16:17:37 -0500

    Tatiana ModeloPersona y clase Persona

--- src/Modelo/ModeloPersona.java
+++ src/Modelo/ModeloPersona.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author tatiana
+ * @author Tatiana Lalvay
  */
 public class ModeloPersona extends Persona {
     ConexionPG conpg = new ConexionPG();
