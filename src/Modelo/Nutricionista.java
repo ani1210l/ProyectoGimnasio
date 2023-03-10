ff679d80c0c520ddcdbb3427342ee66b324990e0
jsari <jsari@DESKTOP-IMNDPBL>
2023-03-09 19:27:00 -0500

    Subido modelo nutricionista

--- src/Modelo/Nutricionista.java
+++ src/Modelo/Nutricionista.java
@@ -11,13 +11,25 @@ public class Nutricionista extends Empleado {
     private int nutricionista_codigo;
     private String rutina;
 
-    public Nutricionista(int empleado_codigo, double salario_emp, int horario_codigo, boolean estado) {
-        super(empleado_codigo, salario_emp, horario_codigo, estado);
+    public Nutricionista() {
+    }
+    
+    
+
+    public Nutricionista(int nutricionista_codigo, String rutina) {
+        this.nutricionista_codigo = nutricionista_codigo;
+        this.rutina = rutina;
     }
 
-    public Nutricionista(int empleado_codigo, double salario_emp, int horario_codigo, boolean estado, int cod_persona, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
-        super(empleado_codigo, salario_emp, horario_codigo, estado, cod_persona, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
+    public Nutricionista(int empleado_codigo, double salario_emp, int horario_codigo, int emp_codper) {
+        super(empleado_codigo, salario_emp, horario_codigo, emp_codper);
     }
+
+    public Nutricionista(int empleado_codigo, double salario_emp, int horario_codigo, int emp_codper, int cod_persona, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
+        super(empleado_codigo, salario_emp, horario_codigo, emp_codper, cod_persona, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
+    }
+
+    
 
     public int getNutricionista_codigo() {
         return nutricionista_codigo;
