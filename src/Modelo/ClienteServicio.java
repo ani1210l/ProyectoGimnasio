a87501d38a77e2f8ae7a636d47d7dbffaf348f24
damian2340842 <123527681+damian2340842@users.noreply.github.com>
2023-03-09 19:25:39 -0500

    damian creoclasemodelocliente damiansolucion erroresmodelocliente damian creacion modelopago damian creacion modeloservcli

--- src/Modelo/ClienteServicio.java
+++ src/Modelo/ClienteServicio.java
@@ -8,15 +8,53 @@ import java.sql.Date;
  * @author Usuario
  */
 public class ClienteServicio extends Cliente{
+private int cli_serv_cod,Servicio_cod,Cod_cliente;
+private Date servcli_fechaingreso,servcli_fechafin;
 
-    public ClienteServicio(int cliente_codigo, Date fecha_ingreso, int cli_codper) {
-        super(cliente_codigo, fecha_ingreso, cli_codper);
+    public ClienteServicio() {
     }
 
-    public ClienteServicio(int cliente_codigo, Date fecha_ingreso, int cli_codper, int cod_persona, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
-        super(cliente_codigo, fecha_ingreso, cli_codper, cod_persona, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
+    public int getCli_serv_cod() {
+        return cli_serv_cod;
     }
 
+    public void setCli_serv_cod(int cli_serv_cod) {
+        this.cli_serv_cod = cli_serv_cod;
+    }
+
+    public int getServicio_cod() {
+        return Servicio_cod;
+    }
+
+    public void setServicio_cod(int Servicio_cod) {
+        this.Servicio_cod = Servicio_cod;
+    }
+
+    public int getCod_cliente() {
+        return Cod_cliente;
+    }
+
+    public void setCod_cliente(int Cod_cliente) {
+        this.Cod_cliente = Cod_cliente;
+    }
+
+    public Date getServcli_fechaingreso() {
+        return servcli_fechaingreso;
+    }
+
+    public void setServcli_fechaingreso(Date servcli_fechaingreso) {
+        this.servcli_fechaingreso = servcli_fechaingreso;
+    }
+
+    public Date getServcli_fechafin() {
+        return servcli_fechafin;
+    }
+
+    public void setServcli_fechafin(Date servcli_fechafin) {
+        this.servcli_fechafin = servcli_fechafin;
+    }
+        
+  
    
     
     
