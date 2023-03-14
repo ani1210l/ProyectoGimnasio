d9c71eae66c645d28f95a8ffa3375d1cc27150fd
damian2340842 <123527681+damian2340842@users.noreply.github.com>
2023-03-14 10:46:31 -0500

    Merge origin/master
    Conflicts:
    	src/Controlador/ControladorCliente.java
    	src/Vista/VistaCliente.java
    ARREGLO CLIENTE Y CONTROLADOR
    DDD

--- src/Controlador/ControladorPrincipal.java
+++ src/Controlador/ControladorPrincipal.java
@@ -56,7 +56,7 @@ public class ControladorPrincipal {
         vistaPrincipal.getjDesktopPane().add(VISTAC);
 
         ControladorCliente controlc = new ControladorCliente(modeloC, VISTAC);
-        controlc.iniciarControl();//Empezamos las escuchas a los eventos de la vista, Listeners.
+        controlc.iniciarControl();//EmpMMMMMMMMezamos las escuchas a los eventos de la vista, Listeners.
 
     }
      
