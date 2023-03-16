d7d6ef70d1d498bf4cd47ba4d8fc89f915920d0b
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-16 01:38:51 -0500

    Clase nutricionista

--- src/modelo/Nutricionista.java
+++ src/modelo/Nutricionista.java
@@ -6,27 +6,24 @@ public class Nutricionista extends Persona {
 
     private int nutri_codigo;
     private int nutri_codper;
-    private String nutri_rutina;
-    private String nutri_horario;
+    private int nutri_aniosExperiencia;
     private String nutri_estado;
 
     public Nutricionista() {
     }
 
-    public Nutricionista(int nutri_codigo, int nutri_codper, String nutri_rutina, String nutri_horario, String nutri_estado) {
+    public Nutricionista(int nutri_codigo, int nutri_codper, int nutri_aniosExperiencia, String nutri_estado) {
         this.nutri_codigo = nutri_codigo;
         this.nutri_codper = nutri_codper;
-        this.nutri_rutina = nutri_rutina;
-        this.nutri_horario = nutri_horario;
+        this.nutri_aniosExperiencia = nutri_aniosExperiencia;
         this.nutri_estado = nutri_estado;
     }
 
-    public Nutricionista(int nutri_codigo, int nutri_codper, String nutri_rutina, String nutri_horario, String nutri_estado, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
+    public Nutricionista(int nutri_codigo, int nutri_codper, int nutri_aniosExperiencia, String nutri_estado, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
         super(per_codigo, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
         this.nutri_codigo = nutri_codigo;
         this.nutri_codper = nutri_codper;
-        this.nutri_rutina = nutri_rutina;
-        this.nutri_horario = nutri_horario;
+        this.nutri_aniosExperiencia = nutri_aniosExperiencia;
         this.nutri_estado = nutri_estado;
     }
 
@@ -46,20 +43,12 @@ public class Nutricionista extends Persona {
         this.nutri_codper = nutri_codper;
     }
 
-    public String getNutri_rutina() {
-        return nutri_rutina;
+    public int getNutri_aniosExperiencia() {
+        return nutri_aniosExperiencia;
     }
 
-    public void setNutri_rutina(String nutri_rutina) {
-        this.nutri_rutina = nutri_rutina;
-    }
-
-    public String getNutri_horario() {
-        return nutri_horario;
-    }
-
-    public void setNutri_horario(String nutri_horario) {
-        this.nutri_horario = nutri_horario;
+    public void setNutri_aniosExperiencia(int nutri_aniosExperiencia) {
+        this.nutri_aniosExperiencia = nutri_aniosExperiencia;
     }
 
     public String getNutri_estado() {
@@ -69,5 +58,4 @@ public class Nutricionista extends Persona {
     public void setNutri_estado(String nutri_estado) {
         this.nutri_estado = nutri_estado;
     }
-
 }
