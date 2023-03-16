ad16e57b24bc88ef6e2447f6e57ae010121118d4
franxxdmsk132 <122382819+franxxdmsk132@users.noreply.github.com>
2023-03-16 14:43:39 -0500

    controlador cliente

--- src/controlador/ControladorCliente.java
+++ src/controlador/ControladorCliente.java
@@ -1,6 +1,13 @@
 package controlador;
 
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import modelo.Cliente;
+import modelo.Instructor;
 import modelo.ModeloCliente;
+import modelo.ModeloPersona;
 import vista.VistaCliente;
 
 public class ControladorCliente {
@@ -15,51 +22,59 @@ public class ControladorCliente {
     }
 
     public void iniciarControl() {
-        //vista.getBtnCrearcliente().addActionListener(l -> abrirjDlgCliente());
-        // vista.getBtnGuardarcliente().addActionListener(l -> crearModificarCliente());
+        vista.getBtnCrear().addActionListener(l -> abrirjDlgCliente());
+        vista.getBtnGuardar().addActionListener(l -> crearModificarCliente());
     }
 
-//    public void abrirjDlgCliente() {
-//
-//        vista.getjDialogCliente().setVisible(true);
-//        vista.getjDialogCliente().setSize(809, 460);
-//        vista.getjDialogCliente().setLocationRelativeTo(null);
-//        vista.getjDialogCliente().setName("Crear nuevo Cliente");
-//        vista.getjDialogCliente().setTitle("Crear nuevo Cliente");
-//
-//    }
+    public void abrirjDlgCliente() {
 
-    /*  public void crearModificarCliente() {
+        vista.getjDlgCliente().setVisible(true);
+        vista.getjDlgCliente().setSize(809, 460);
+        vista.getjDlgCliente().setLocationRelativeTo(null);
+        vista.getjDlgCliente().setName("Crear nuevo Cliente");
+        vista.getjDlgCliente().setTitle("Crear nuevo Cliente");
 
-        if ("Crear nuevo Cliente".equals(vista.getjDialogCliente().getName())) {
+    }
+    public void cargarTablaDeCliente() {
+        DefaultTableModel tabla = (DefaultTableModel) vista.getTblCliente().getModel();
+        tabla.setNumRows(0);
+
+        List<Cliente> instructores = modelo.listaClientesTabla();
+        instructores.stream().forEach(i -> {
+            String[] datos = {String.valueOf(i.getCliente_codigo()),i.getPer_cedula(),i.getPer_nombre(),i.getPer_apellido(),i.getPer_telefono(),i.getPer_telefono(),(String.valueOf(i.getCliente_tipomembresia()))};
+            tabla.addRow(datos);
+        });
+    }
+
+      public void crearModificarCliente() {
+
+        if ("Crear nuevo Cliente".equals(vista.getjDlgCliente().getName())) {
 
             //Validar datos
             ModeloPersona persona = new ModeloPersona();
-            if (persona.validarRepetidos(vista.getTblCliente().getText()) == 0) {
+            if (persona.validarRepetidos(vista.getTxtCedula().getText()) == 0) {
 
                 //Setear datos de persona
-                persona.setPer_cedula(vista.getTxtCedulacliente().getText());
-                persona.setPer_nombre(vista.getTxtNombrecli().getText());
-                persona.setPer_apellido(vista.getTxtApellidocli().getText());
-                persona.setPer_telefono(vista.getTxtTelefonoclie().getText());
-                persona.setPer_direccion(vista.getTxtDireccionclie().getText());
+                persona.setPer_cedula(vista.getTxtCedula().getText());
+                persona.setPer_nombre(vista.getTxtNombre().getText());
+                persona.setPer_apellido(vista.getTxtApellido().getText());
+                persona.setPer_telefono(vista.getTxtTelefono().getText());
+                persona.setPer_direccion(vista.getTxtDireccion().getText());
 
-                Date fecha = vista.getFechaDeNacimientoclie().getDate();
+                Date fecha = vista.getFechaDeNacimiento().getDate();
                 java.sql.Date fechaSQL = new java.sql.Date(fecha.getTime());
                 persona.setPer_fechaNac(fechaSQL);
                 
 
-                //Setear datos de instructor
-              
+                //Setear datos de cliente
+                modelo.setCliente_tipomembresia(vista.getComboMembresia().getSelectedItem().toString());
                 }
-
-                modelo.setIns_horario(horario);
 
                 if (persona.crearPersona()) {
                     //Guarda el codigo de la persona
                     System.out.println("Persona");
-                    modelo.setIns_codper(persona.traerCodigoDePersonaCreada());
-                    if (modelo.crearInstructor()) {
+                    modelo.setCliente_codper(persona.traerCodigoDePersonaCreada());
+                    if (modelo.crearCliente()) {
                         JOptionPane.showMessageDialog(null, "Se creo exitosamente");
                     } else {
                         JOptionPane.showMessageDialog(null, "No se pudo registrar");
@@ -70,5 +85,5 @@ public class ControladorCliente {
             } else {
                 JOptionPane.showMessageDialog(null, "El numero de cedula ya se encuentra registrado en la base de datos");
             }
-        }*/
+        }
 }
