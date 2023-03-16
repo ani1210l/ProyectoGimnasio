53531bb7bd176c347a207e725217b3810587aee5
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-16 01:44:17 -0500

    Modelo nutricionista

--- src/modelo/ModeloNutricionista.java
+++ src/modelo/ModeloNutricionista.java
@@ -9,16 +9,16 @@ public class ModeloNutricionista extends Nutricionista {
     public ModeloNutricionista() {
     }
 
-    public ModeloNutricionista(int nutri_codigo, int nutri_codper, String nutri_rutina, String nutri_horario, String nutri_estado) {
-        super(nutri_codigo, nutri_codper, nutri_rutina, nutri_horario, nutri_estado);
+    public ModeloNutricionista(int nutri_codigo, int nutri_codper, int nutri_aniosExperiencia, String nutri_estado) {
+        super(nutri_codigo, nutri_codper, nutri_aniosExperiencia, nutri_estado);
     }
 
-    public ModeloNutricionista(int nutri_codigo, int nutri_codper, String nutri_rutina, String nutri_horario, String nutri_estado, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
-        super(nutri_codigo, nutri_codper, nutri_rutina, nutri_horario, nutri_estado, per_codigo, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
+    public ModeloNutricionista(int nutri_codigo, int nutri_codper, int nutri_aniosExperiencia, String nutri_estado, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
+        super(nutri_codigo, nutri_codper, nutri_aniosExperiencia, nutri_estado, per_codigo, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
     }
 
     public boolean crearNutricionista() {
-        String sql = "INSERT INTO nutricionista(nutri_codper, nutri_rutina, nutri_horario, nutri_estado)VALUES (" + getNutri_codper() + ", " + getNutri_horario() + "', 'A');";
+        String sql = "INSERT INTO nutricionista(nut_codper, nut_aniosexperiencia, nutri_estado)VALUES (" + getNutri_codper() + ", " + getNutri_aniosExperiencia()+ "', 'A');";
 
         return conpg.accion(sql);
     }
